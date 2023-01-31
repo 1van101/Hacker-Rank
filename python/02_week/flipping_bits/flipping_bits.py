c28e5b1ea7b4e6e718da975df767f97a2b9dae78
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-01-31 13:34:23 +0200

    added

---
+++ python/02_week/flipping_bits/flipping_bits.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    flipped_n = ''
+    new_n = f'{n:032b}'
+
+    for n in new_n:
+        if n == '0':
+            flipped_n += '1'
+        else:
+            flipped_n += '0'
+
+    return int(flipped_n, 2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
