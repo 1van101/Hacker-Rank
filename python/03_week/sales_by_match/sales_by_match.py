72f962832c4565f69ac0fb5af67e932dd76c0be0
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-04-21 16:53:01 +0300

    replaced

--- python/03_week/sales_by_match/sales_by_match.py
+++ python/03_week/sales_by_match/sales_by_match.py
@@ -1,5 +1,19 @@
-import collections
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
 
 def sockMerchant(n, ar):
     socks_colors = {}
@@ -12,9 +26,15 @@ def sockMerchant(n, ar):
     socks_colors = [value // 2 for value in socks_colors.values() if value > 1]
     return sum(socks_colors)
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    n = int(input().strip())
 
+    ar = list(map(int, input().rstrip().split()))
 
+    result = sockMerchant(n, ar)
 
+    fptr.write(str(result) + '\n')
 
-print(sockMerchant(5, [10, 20, 20, 10, 10, 30, 50, 10, 20]))
+    fptr.close()
