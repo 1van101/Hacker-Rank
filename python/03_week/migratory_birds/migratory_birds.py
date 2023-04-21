9eb2e0a4e4cd88a529d485eb5373898feee93065
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-04-21 16:59:30 +0300

    commit

---
+++ python/03_week/migratory_birds/migratory_birds.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    birds = {}
+
+    for bird in arr:
+        if bird not in birds:
+            birds[bird] = 0
+
+        birds[bird] += 1
+
+    birds_filtered = {x: y for x, y in birds.items() if y == max(birds.values())}
+    return min(birds_filtered)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
